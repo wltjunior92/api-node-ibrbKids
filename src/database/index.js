03bd57647751f14ae11f337ae4f41e4ed38b6d65
Walter Junior <wlt.junior92@gmail.com>
2020-02-28 18:11:54 -0300

    Correçõ das rotas e do arquivo index

--- src/database/index.js
+++ src/database/index.js
@@ -4,15 +4,24 @@ const dbConfig = require('../config/database');
 const Usuario = require('../models/Usuario');
 const Entidade = require('../models/Entidade');
 const Evento = require('../models/Evento');
+const Crianca = require('../models/Crianca');
+const Contato = require('../models/Contato');
+const Endereco = require('../models/Endereco');
 
 const connection = new Sequelize(dbConfig);
 
 Usuario.init(connection);
 Entidade.init(connection);
 Evento.init(connection);
+Crianca.init(connection);
+Contato.init(connection);
+Endereco.init(connection);
 
-Usuario.associate(connection.models);
 Entidade.associate(connection.models);
+Usuario.associate(connection.models);
 Evento.associate(connection.models);
+Crianca.associate(connection.models);
+Contato.associate(connection.models);
+Endereco.associate(connection.models);
 
 module.exports = connection;
--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,9 @@
 const express = require('express');
 const UsuarioController = require('./controllers/UsuarioController');
-const MinisterioController = require('./controllers/MinisterioController');
-const EventoController = require('./controllers/EventoController');
 
 const routes = express.Router();
 
-// Usuario
-// Buscar todos por ministerio
-routes.get('/ministerios/:ministerio_id/usuarios', UsuarioController.index);
 // Criar Usuario
-routes.post('/ministerios/:ministerio_id/usuarios', UsuarioController.store);
-
-// Ministerio
-// Buscar Todos
-routes.get('/ministerios', MinisterioController.index);
-// Criar Ministerio
-routes.post('/ministerios', MinisterioController.store);
-
-// Evento
-// Buscar todos por ministerio
-routes.get('/eventos', EventoController.index);
-// Criar Evento
-routes.post('/usuarios/:usuario_id/eventos', EventoController.store);
+routes.post('/ministerios/usuarios', UsuarioController.store);
 
 module.exports = routes;
